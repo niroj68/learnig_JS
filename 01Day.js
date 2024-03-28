c6772823b7bb6b0540c3efd6aec39350b0ba042e
Niroj Kumar Naik <nirojnaik68@gmail.com>
2024-03-27 20:22:48 -0700

    my first js file taught by Hitesh sir from Chai aur Code

---
+++ 01Day.js
@@ -0,0 +1,22 @@
+// console.log("hello")
+
+const accountId = 133244
+let accountEmail = "nirojnaik68@gmail.com"
+var accountPassword = "2356"
+accountCity = "Baripada"
+let accountState ;
+// accountId can not be change
+
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "nirojnaik12@gmail.com"
+accountCity = "Bhubaneswer"
+accountPassword = "3245"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use ver due to issue in block and functional scoope
+*/
+
