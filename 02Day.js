4ca325514d18b3ee644600e3b5f0a2c957d0308f
Niroj Kumar Naik <nirojnaik68@gmail.com>
2024-03-29 14:20:45 -0700

    hitesh sir 5th tutorial Data type

---
+++ 02Day.js
@@ -0,0 +1,31 @@
+"use strictl";// treat all JS code as newer version
+
+// alert("hello") //we are using nodejs not browser
+
+console.log(34+5);
+console.log("Niroj"); //code readability should be maintain
+
+// console.log(34
+//     +5);
+// console.log("Niroj"); //Dont do this
+
+console.log(34+5);console.log("Niroj"); // It is runable but still Dont do this it effects the readability
+
+let name = "Niroj"
+let age = 22
+let didYouEat = true
+let state
+
+console.table([name,age,didYouEat,state])
+
+// number = 2^53
+// bigint
+// string -> use "" 
+// boolean -> true or false
+// null -> standalone value a type
+// undefine ->
+// symbol -> unique
+// object ->
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
