ad98333f86877f69e3c39910b0b1cb8471f354c7
Niroj Kumar Naik <nirojnaik12@gmail.com>
2024-04-03 21:04:29 +0530

    comparison operator is a mess in JS

---
+++ 05Day.js
@@ -0,0 +1,26 @@
+/* --------------------------compare--------------------- */
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 <= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+
+console.log("2" >= 1)// we are expecting of NaN but string convered to number
+//null 
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null > 0)
+console.log(null < 0)
+console.log(null <= 0)
+//undefined
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined <= 0)
+
+console.log( "2" === 2 )
+
+// the above examples are worehless to use in industries so try to avoid in practical use but good for exams to confuse you
